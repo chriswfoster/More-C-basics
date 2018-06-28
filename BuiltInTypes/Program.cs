61b5fa91c0aff89fb235b07d185b7abdf8d12772
Chris <chriswfoster@gmail.com>
2018-06-28 14:01:13 -0500

    First

---
+++ BuiltInTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        bool b = true; // boolean variable can strictly only hold true or false
+        byte byter = 155; // bytes can support number range from 0-255 (256 full range). sbyte is -128 to 127. 
+        int i = 0;
+        Console.WriteLine("Min = {0}", int.MinValue); // bye typing int. , I can see the properties on the integer to see what I have access to with that data type.
+        double d = 123.3434; // a type of floating.
+        Console.WriteLine(d);
+        }
+    }
---
+++ IntroToCSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+// a name space is a way to organize your code, it's a collection of classes, deligates, other namespaces, etc.
+// not using a namespace here.
+
+    class Program
+    { 
+        static void Main2()
+        {
+         string username = Console.ReadLine();
+   
+        // Console.WriteLine("Your name is " + username); // this is concatenation
+        //  Console.WriteLine("Your name is {0}", username); // this is placeholder syntax, basically references a paramter passed in after the string.
+        // Console.WriteLine($"Your name is {username}"); // I think this is string interpolation
+        Main3(ref username); // Here I'm passuing in username, ref (referencing) the username variable.
+        }
+
+        static void Main3(ref string username) // I told C# to reference the string username that I'm passing in from Main2
+    {
+        Console.WriteLine("Now type your last name:");
+        string lastname = Console.ReadLine();
+        Console.WriteLine("Hello, {0} {1}!", username, lastname);
+    }
+
+
+        static void Main() // this is a function/method.
+        {
+        Console.WriteLine("Please type your first name:"); // full name is actually System.Console, it gets it's functionality. If you hover over console, you can see the System class.
+        Main2(); // this will call the previous function.
+        }
+    }
